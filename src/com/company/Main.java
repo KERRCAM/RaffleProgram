7352b73a9b9cce838b232bfc0d469a58f40c83ae
KERRCAM <kerrcameron@hotmail.com>
2021-11-23 22:17:41 +0000

    ticketGot method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,12 @@
 package com.company;
-//did this work?
+import java.util.Random;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
 
-    static String ticketHolders[][] = new String[2][100];
+    static String ticketHoldersNames[] = new String[100];
+    static int ticketHoldersTickets[] = new int[100];
     static int ticketsAvailable[] = new int[100];
 
     public static String getInput(String prompt) {
@@ -22,10 +23,23 @@ public class Main {
     }
 
     public static void getTicket(){
-
+        Random random = new Random();
+        String name = getInput("what is your first name?");
+        boolean ticketGot = false;
+        while (ticketGot == false) {
+            int ticket = random.nextInt(100);
+            if (ticketsAvailable[ticket] == -1) {
+                System.out.println(". . .");
+            }else {
+                ticketHoldersNames[ticket] = name;
+                ticketHoldersTickets[ticket] = ticket;
+                ticketGot = true;
+            }
+        }
     }
 
     public static void checkTicket() {
+        String name = getInput("what is your first name?");
 
     }
 
@@ -34,9 +48,6 @@ public class Main {
 
     private static void menu() {
         String answer = "";
-        for (int i = 0; i < 100; i++) {
-            ticketsAvailable[i] = i;
-        }
         try {
             while (true) {
                 answer = getInput("would you like to enter a ticket number or get a ticket? (type 'enter' or 'get'");
@@ -65,6 +76,9 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            ticketsAvailable[i] = i;
+        }
         boolean run = true;
         while (run == true) {
             String again = getInput("would you like to enter/get another ticket");
@@ -74,8 +88,7 @@ public class Main {
             if (again.equals("yes")) {
                 run = false;
             }
-        }
-    }
+        }}
 
 
 
